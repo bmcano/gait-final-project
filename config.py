911c7a53f239040992fae342b273a9d4a96d63d6
Brandon Cano <brandoncano01@gmail.com>
2024-11-21 13:04:52 -0600

    added config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# Project Settings
+
+# To save money on API calls when we test the program itself use this for hard-coded responses.
+# Otherwise when testing AI calls set this to false. 
+DEMO_MODE = True 
+
